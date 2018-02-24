1ef6561ea9fdd5929469fb02d166df8672e76db2
易波 <yibo@feinno.com>
2018-02-24 18:52:28 +0800

    test env

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @MapperScan(
         {
                 "com.example.demo.mapper"
@@ -11,12 +14,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 "com.example.demo.mapperxml"
         })
 @SpringBootApplication
-public class DemoApplication {
+public class Demo1Application {
 
     public static void main(String[] args) {
 
         System.out.println("hi");
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Demo1Application.class, args);
 
         System.out.println("end hi");
 
